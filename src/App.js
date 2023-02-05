78361e9dd02231b5a6a9828d39af9f1fcf10b726
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-02-05 18:14:21 -0500

    Configurando REDUX, instalando dev tools y creando el store

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import { BrowserRouter as Router, Route, Switch  } from "react-router-dom";
-import Header from "./components/Header";
-import ProductList from "./components/ProductList";
-import ProductCreate from "./components/ProductCreate";
-import ProductUpdate from "./components/ProductUpdate";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import ProductCreate from './components/ProductCreate';
+import ProductUpdate from './components/ProductUpdate';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
     <Router>
-      <Header />
-      <div className="container mt-5">
-        <Switch>
-          <Route exact path="/" component={ProductList} />
-          <Route exact path="/product/create" component={ProductCreate} />
-          <Route exact path="/product/update/:id" component={ProductUpdate} />
-        </Switch>
-      </div>
+      <Provider store={store}>
+        <Header />
+        <div className="container mt-5">
+          <Switch>
+            <Route exact path="/" component={ProductList} />
+            <Route exact path="/product/create" component={ProductCreate} />
+            <Route exact path="/product/update/:id" component={ProductUpdate} />
+          </Switch>
+        </div>
+      </Provider>
     </Router>
   );
 }
