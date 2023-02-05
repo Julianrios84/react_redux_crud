ff2afa2c1a92781ac799c6871ba5ce57826f617a
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-02-05 16:50:03 -0500

    Agregando React Router DOM y Componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Route, Switch  } from "react-router-dom";
+import Header from "./components/Header";
+import ProductList from "./components/ProductList";
+import ProductCreate from "./components/ProductCreate";
+import ProductUpdate from "./components/ProductUpdate";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
+    <Router>
+      <Header />
+      <div className="container mt-5">
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route exact path="/product/create" component={ProductCreate} />
+          <Route exact path="/product/update/:id" component={ProductUpdate} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
       <div className="container">
-        <h1>CRUD - React, Redux, REST API & Axios </h1>
+        <h1>
+          <Link to={'/'} className="text-light">
+            CRUD - React, Redux, REST API & Axios
+          </Link>
+        </h1>
       </div>
-      <a
+      <Link
         className="btn btn-danger nuevo-post d-block d-md-inline-block"
-        href="/product/add"
+        to={'/product/add'}
       >
         Agregar Producto &#43;
-      </a>
+      </Link>
     </div>
   );
 };
