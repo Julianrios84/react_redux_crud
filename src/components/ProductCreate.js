4a3540a93fbeb5da96bdc0a1bd8de74b7d735a18
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-02-05 16:56:48 -0500

    Creando el Formulario para Nuevos Productos~

--- src/components/ProductCreate.js
+++ src/components/ProductCreate.js
@@ -1,8 +1,43 @@
-
 const ProductCreate = () => {
   return (
-    <div>ProductCreate</div>
-  )
-}
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Agregar nuevo producto
+            </h2>
+            <form className="">
+              <div className="form-group">
+                <label htmlFor="">Nombre producto</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre producto"
+                  name="name"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="">Precio producto</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Precio producto"
+                  name="price"
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Agregar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ProductCreate
+export default ProductCreate;
