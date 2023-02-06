009bb65f335f64f5253c7dcd25f8ab0494098ea6
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-02-05 19:16:24 -0500

    Definiendo el action y utilizando useDispatch

--- src/actions/product.actions.js
+++ src/actions/product.actions.js
@@ -1,3 +1,9 @@
 import {
   CREATE_PRODUCT, CREATE_PRODUCT_SUCCESS, CREATE_PRODUCT_ERROR
 } from '../types'
+
+export function createProductAction() {
+  return () => {
+    console.log('createProductAction')
+  }
+}
