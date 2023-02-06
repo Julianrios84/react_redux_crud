87e18a49e47e52fdd2cbf58da7c9d4c63506d462
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-02-05 19:06:32 -0500

    Definiendo las primeras acciones de Redux01~

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import ProductList from './components/ProductList';
 import ProductCreate from './components/ProductCreate';
@@ -9,18 +9,18 @@ import store from './store';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Provider store={store}>
         <Header />
         <div className="container mt-5">
-          <Switch>
-            <Route exact path="/" component={ProductList} />
-            <Route exact path="/product/create" component={ProductCreate} />
-            <Route exact path="/product/update/:id" component={ProductUpdate} />
-          </Switch>
+          <Routes>
+            <Route exact path="/" element={<ProductList/>} />
+            <Route exact path="/product/create" element={<ProductCreate/>} />
+            <Route exact path="/product/update/:id" element={<ProductUpdate/>} />
+          </Routes>
         </div>
       </Provider>
-    </Router>
+    </BrowserRouter>
   );
 }
 
